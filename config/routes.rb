c8f887b368fe5a50e20cef3f60beb380cf36435a
Yuta-Asano <70369660+Yuta-Asano@users.noreply.github.com>
2020-11-14 17:34:03 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
-  get 'tweets/show'
-  devise_for :users
+  resources :users, only: [:index, :show]
+  resources :tweets, only: [:index, :show, :create]
+
+  root 'tweets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
