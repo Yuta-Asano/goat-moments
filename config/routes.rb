3038bb9f9c4d3ffbc8ad31ea80478750969eb014
Yuta-Asano <70369660+Yuta-Asano@users.noreply.github.com>
2020-11-16 13:07:11 +0900

    新規登録、ログイン、ログアウト機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show]
-  resources :tweets, only: [:index, :show, :create]
-
-  root 'tweets#index'
+  devise_for :users
+  root to: 'tweets#index'
+  resources :tweets do
+    resources :comments, only: :create
+    collection do
+      get 'search'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
