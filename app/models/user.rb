0a2092f57a7912c744061f07d27bc935947848af
Yuta-Asano <70369660+Yuta-Asano@users.noreply.github.com>
2020-11-24 14:11:27 +0900

    微編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tweets, dependent: :destroy
   validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, presence: true, with: PASSWORD_REGEX
+
+     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+     validates :email, { presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } }
+  end
 end
